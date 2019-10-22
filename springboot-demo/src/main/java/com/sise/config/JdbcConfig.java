29b9ff484e0ea3eb17810e695e1ec95362ed4b74
tlimited <706548532@qq.com>
2019-10-22 18:38:04 +0800

    java springboot推荐的@ConfigurationProperties注入方式

--- springboot-demo/src/main/java/com/sise/config/JdbcConfig.java
+++ springboot-demo/src/main/java/com/sise/config/JdbcConfig.java
@@ -2,12 +2,14 @@ package com.sise.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
-
+/*
+方式1
 @Configuration
 @PropertySource("classpath:jdbc.properties")
 public class JdbcConfig {
@@ -30,4 +32,17 @@ public class JdbcConfig {
         dataSource.setPassword(password);
         return dataSource;
     }
+}*/
+
+/**
+ * 方式2
+ */
+@Configuration
+@EnableConfigurationProperties(JdbcProperties.class)
+public class JdbcConfig {
+
+//    private JdbcProperties prop;
+//    public JdbcConfig(JdbcProperties prop){
+//        this.prop = prop;
+//    }
 }
--- springboot-demo/src/main/java/com/sise/controller/TestController.java
+++ springboot-demo/src/main/java/com/sise/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.sise.controller;
 
+import com.sise.config.JdbcProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +10,19 @@ import javax.sql.DataSource;
 @RestController
 public class TestController {
 
+/*
     @Autowired
     private DataSource dataSource;
+*/
+
+    private JdbcProperties prop;
+    public TestController(JdbcProperties prop){
+        this.prop = prop;
+    }
 
     @GetMapping("hello")
     public String hello(){
-        System.out.println(dataSource);
+        System.out.println(prop);
         return "hello, spring boot!";
     }
 }
